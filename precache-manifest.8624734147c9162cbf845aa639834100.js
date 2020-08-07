734a812e282146ed6a54fca2d70fcd2116c79fb0
TARUN KISHORE <tarunksu30@gmail.com>
2020-08-07 17:39:07 +0530

    Updates

---
+++ precache-manifest.8624734147c9162cbf845aa639834100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f785573984ff27e3d990902082588b19",
+    "url": "/tarunkishore2303/CovidApp/index.html"
+  },
+  {
+    "revision": "8f3b96452f96f526f135",
+    "url": "/tarunkishore2303/CovidApp/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "f3a1c451e7213709fa4c",
+    "url": "/tarunkishore2303/CovidApp/static/css/main.c62d7d5f.chunk.css"
+  },
+  {
+    "revision": "8f3b96452f96f526f135",
+    "url": "/tarunkishore2303/CovidApp/static/js/2.74ef2ba9.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/tarunkishore2303/CovidApp/static/js/2.74ef2ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3a1c451e7213709fa4c",
+    "url": "/tarunkishore2303/CovidApp/static/js/main.5d1800f1.chunk.js"
+  },
+  {
+    "revision": "5e71fa2f32dca36acd3e",
+    "url": "/tarunkishore2303/CovidApp/static/js/runtime-main.63bfd41f.js"
+  }
+]);
